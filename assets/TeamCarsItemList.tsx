808295a17cca7d5485c2cfa221f0a309064e15ce
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-11-01 03:22:38 +0300

    Application | Updated

--- assets/TeamCarsItemList.tsx
+++ assets/TeamCarsItemList.tsx
@@ -1,4 +1,4 @@
-import { TeamEmily, TeamJane, TeamJohn, TeamMichael } from "@/icons/team-card-picture";
+import { TeamPhoto } from "@/icons/team-card-picture";
 
 export interface TeamItem {
     icon:React.ReactNode | React.ReactNode[],
@@ -9,25 +9,25 @@ export interface TeamItem {
 
 export const TeamItemList:TeamItem[] = [
     {
-        icon:<TeamJohn/>,
+        icon:<TeamPhoto imageSrc="https://picsum.photos/200/300"/>,
         name:"John Smith",
         position:"CEO and Founder",
         content:"10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
     },
     {
-        icon:<TeamJane/>,
+        icon:<TeamPhoto imageSrc="https://picsum.photos/200/300"/>,
         name:"Jane Doe",
         position:"Director of Operations",
         content:"10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
     },
     {
-        icon:<TeamMichael/>,
+        icon:<TeamPhoto imageSrc="https://picsum.photos/200/300"/>,
         name:"Michael Brown",
         position:"Senior SEO Specialist",
         content:"5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization",
     },
     {
-        icon:<TeamEmily/>,
+        icon:<TeamPhoto imageSrc="https://picsum.photos/200/300"/>,
         name:"Emily Johnson",
         position:"PPC Manager",
         content:"3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis",
