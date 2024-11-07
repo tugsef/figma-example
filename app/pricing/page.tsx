d3ec9a70f8cb49ebda5ceb576d90c5b9ec56f951
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-11-07 13:48:06 +0300

    version 1.1.0

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,17 +1,24 @@
+import HyperText from "@/components/ui/hyper-text";
 import Link from "next/link";
 import React from "react";
 
 function page() {
   return (
-    <div className="w-full h-screen bg-green text-xl lg:text-5xl tracking-widest flex justify-center items-center flex-col gap-4 font-semibold">
-      <h1>Coming Soon</h1>
-      <Link
-        href="/"
-        className="text-dark/50 font-light  lg:text-xl hover:border-b border-dark"
-      >
-        Home Page
-      </Link>
-    </div>
+    <div className="w-full h-screen bg-green text-2xl lg:text-5xl tracking-widest flex justify-center items-center flex-col  font-semibold">
+    <HyperText
+    text="Coming Soon"
+/> 
+  <Link
+    href="/"
+    className="font-light text-xs  hover:border-b border-dark"
+
+  >
+    <HyperText
+    text="Home Page"
+    className="cursor-pointer"/>
+    
+  </Link>
+</div>
   );
 }
 
